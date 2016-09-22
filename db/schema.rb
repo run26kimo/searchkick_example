631587db977d3a2e6f615c7fc6ac88ba72517f68
Rich Ke <run26kimo@gmail.com>
2016-09-22 15:58:29 +0800

    feat(DB): use pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160920131551) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
